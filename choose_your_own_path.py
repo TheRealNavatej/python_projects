34d65f620d92eb2eb8a8a25fd398f1297963b9ec
TheRealNavatej <tejrocks.925@gmail.com>
2025-10-19 16:57:52 +0530

    Refactor adventure game logic for clarity and flow

--- choose_your_own_path.py
+++ choose_your_own_path.py
@@ -1,50 +1,68 @@
-name = input("Type your name: ")
-print("Welcome", name, "to this adventure!")
+while True:
+    name = input("Type your name: ")
+    print("Welcome", name, "to this adventure!")
 
-answer = input(
-    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
-
-if answer == "left":
     answer = input(
-        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+        "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? "
+    ).lower()
 
-    if answer == "swim":
-        print("You swam acrross and were eaten by an alligator.")
-    elif answer == "walk":
-        print("You walked for many miles, ran out of water and you lost the game.")
-    else:
-        print('Not a valid option. You lose.')
-
-elif answer == "right":
-    answer = input(
-        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back/do nothing)? ").lower()
-
-    if answer == "back":
-        print("You go back and lose.")
-    elif answer == "cross":
+    if answer == "left":
         answer = input(
-            "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+            "You come to a river, you can walk around it or swim across. Type 'walk' to walk around or 'swim' to swim across: "
+        ).lower()
 
-        if answer == "yes":
-            print("You talk to the stanger and they give you gold. You WIN!")
-        elif answer == "no":
-            print("You ignore the stranger and they are offended and you lose.")
+        if answer == "swim":
+            print("You swam across and were eaten by an alligator.")
+        elif answer == "walk":
+            print("You walked for many miles, ran out of water, and lost the game.")
         else:
-            print('Not a valid option. You lose.')
+            print("Not a valid option. You lose.")
+
+    elif answer == "right":
+        answer = input(
+            "You come to a bridge. It looks wobbly. Do you want to cross it or head back (cross/back/do nothing)? "
+        ).lower()
+
+        if answer == "back":
+            print("You go back and lose.")
+        elif answer == "cross":
+            answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+            if answer == "yes":
+                print("You talk to the stranger and they give you gold. You WIN!")
+            elif answer == "no":
+                print("You ignore the stranger and they are offended. You lose.")
+            else:
+                print("Not a valid option. You lose.")
+        elif answer == "do nothing":
+            answer = input(
+                "You stood around for too long. Do you still want to do nothing? (yes/no/eat 5stars): "
+            ).lower()
+            if answer == "yes":
+                print("You stood there until you starved. You lose.")
+            elif answer == "no":
+                print("You decided to make a move and got lost. You lose.")
+            elif answer == "eat 5stars":
+                print("You ate 5 stars and gained superpowers. You WIN!")
+            else:
+                print("Not a valid option. You lose.")
+        else:
+            print("Not a valid option. You lose.")
+
+    elif answer == "do nothing":
+        answer = input("You stood around for too long. Do you still want to do nothing? (yes/no/eat 5stars): ").lower()
+        if answer == "yes":
+            print("You stood there until you starved. You lose.")
+        elif answer == "no":
+            print("You decided to make a move and got lost. You lose.")
+        elif answer == "eat 5stars":
+            print("You ate 5 stars and gained superpowers. You WIN!")
+        else:
+            print("Not a valid option. You lose.")
+
     else:
-        print('Not a valid option. You lose.')
+        print("Not a valid option. You lose.")
 
-elif answer == "do nothing":
-    print("You stood around for too long.do you still want to do nothing? (yes/no/eat 5stars) ").lower()
-    answer = input()
-    if answer == "yes":
-        print("You stood there until you starved. You lose.")
-    elif answer == "no":
-        print("You decided to make a move and got lost. You lose.")
-    elif answer == "eat 5stars":
-        print("You ate 5 stars and gained super powers. You WIN!")
-
-else:
-    print('Not a valid option. You lose.')
-
-print("Thank you for trying", name)
+    retry = input("Do you want to try again? (yes/no): ").lower()
+    if retry != "yes":
+        print("Thanks for playing! Goodbye.")
+        break
